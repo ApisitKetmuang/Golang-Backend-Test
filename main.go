397f32180ea03be117e9c4c8e0aaed3372f458c0
Apisit <apisit.ketmuang@gmail.com>
2024-05-11 22:56:23 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+)
+
+const (
+	/* postgres://posts:38S2GPNZut4Tmvan@dev.opensource-technology.com:5523/posts?sslmode=disable */
+	host     = "dev.opensource-technology.com"  
+	port     = 5523         
+	user     = "posts "     
+	password = "38S2GPNZut4Tmvan" 
+	dbname   = "posts " 
+)
+
+type Post struct {
+	ID       	string 		`json:"id"`
+	Title 		string 		`json:"title"`
+	Content 	string 		`json:"content"`
+	Published 	bool 		`json:"published"`
+	CreatedAt   time.Time 	`json:"created_at"`
+}
+
+var db *sql.DB
+
+func main() {
+
+	psqlInfo := fmt.Sprintf(`host=%s port=%d user=%s password=%s dbname=%s sslmode=disable`,
+	host, 	port, 	user, 	password, 	dbname)
+
+	sdb, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db = sdb
+
+	defer db.Close()
+	
+	err= db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := fiber.New()
+
+	app.Get("/post", getPostsHandler)
+	app.Get("/draft", getDraftsHandler)
+	app.Get("/post/:id", getPostHandler)
+	// app.Get("/post/:id?/:title?", getPostHandler)
+
+	app.Post("/post", createPostHandler)
+	app.Put("/post/:id", updatePostHandler)
+	app.Delete("/post/:id", deletePostHandler)
+
+
+	app.Listen(":8080")
+}
+
+//draft
+func getDraftsHandler(c *fiber.Ctx) error {
+	post, err := getDraft()
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	return c.JSON(post)
+}
+
+//published
+func getPostsHandler(c *fiber.Ctx) error {
+	post, err := getPosts()
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	return c.JSON(post)
+}
+
+//getPostById
+func getPostHandler(c *fiber.Ctx) error {
+	postId := c.Params("id")
+	// title := c.Params("title")
+	post, err := getPost(postId)
+
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	return c.JSON(post)
+}
+
+//CratePostById
+func createPostHandler(c *fiber.Ctx) error {
+	p := new(Post)
+
+	if err := c.BodyParser(p); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	err := createPost(p)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	
+	return c.JSON(p)
+}
+
+//UpdatePostById
+func updatePostHandler(c *fiber.Ctx) error {
+	postId := c.Params("id")
+	p := new(Post)
+
+	if err := c.BodyParser(p); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	post := updatePost(postId, p)
+	return c.JSON(post)
+}
+
+//DeletePostById
+func deletePostHandler(c *fiber.Ctx) error {
+	postId := c.Params("id")
+	err := deletePost(postId)
+
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	return c.SendStatus(fiber.StatusNoContent)
+}
+
---
+++ models.go
@@ -0,0 +1,117 @@
+package main
+
+func createPost(post *Post ) error {
+	_, err := db.Exec(
+		"INSERT INTO public.posts(title, content) VALUES ($1, $2) WHERE title IS NOT NULL;",
+		post.Title,
+		post.Content,
+	)
+		return err
+}
+
+func getPost(id string) (Post, error) {
+	var p Post
+	row := db.QueryRow(
+		"select id, title, content, published, created_at From public.posts WHERE id=$1;",
+		id,
+	)
+
+	err := row.Scan(&p.ID, &p.Title, &p.Content, &p.Published, &p.CreatedAt)
+
+	if err != nil {
+		return Post{}, err 
+	}
+
+	return p, nil
+}
+
+func getPosts() ([]Post, error) {
+	rows, err := db.Query("SELECT id, title, content, published from public.posts WHERE published = true;")
+
+	if err != nil {
+		return nil, err
+	}
+
+	var posts []Post
+	for rows.Next() {
+		var p Post
+		err := rows.Scan(&p.ID, &p.Title, &p.Content, &p.Published)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, p)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
+
+func getDraft() ([]Post, error) {
+	rows, err := db.Query("SELECT id, title, content, published from public.posts WHERE published = false;")
+
+	if err != nil {
+		return nil, err
+	}
+
+	var posts []Post
+	for rows.Next() {
+		var p Post
+		err := rows.Scan(&p.ID, &p.Title, &p.Content, &p.Published)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, p)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
+
+// func getPosts() ([]Post, error) {
+// 	rows, err := db.Query("SELECT id, title, content from public.posts;")
+
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	var posts []Post
+// 	for rows.Next() {
+// 		var p Post
+// 		err := rows.Scan(&p.ID, &p.Title, &p.Content)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		posts = append(posts, p)
+// 	}
+
+// 	if err = rows.Err(); err != nil {
+// 		return nil, err
+// 	}
+
+// 	return posts, nil
+// }
+
+func updatePost(id string, post *Post ) error {
+	_, err := db.Exec(
+		"UPDATE public.posts SET title=$1, content=$2, published=$3 WHERE id =$4;",
+		post.Title,
+		post.Content,
+		post.Published,
+		id,
+	)
+	return err
+}
+
+func deletePost(id string) error {
+	_, err := db.Exec(
+		"DELETE FROM public.posts WHERE id=$1;",
+		id,
+	)
+	return err
+}
